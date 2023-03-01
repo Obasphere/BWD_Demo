b48562c3f4705f73c3ea99b93d72f668d44f439e
Damilola Oba Adesanya <dami.d.adesanya@gmail.com>
2023-03-01 22:22:55 +0100

    Add project files.

---
+++ BWD_Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWD_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Jackline = 5;
+            Jackline = 2 + 2;
+            int mariam = 8;
+            var precious = 9;
+            string faith = "abiodun";
+            DateTime Dami = DateTime.UtcNow;
+            DateTime faridah = DateTime.Now;
+            if (Dami >= faridah)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+            else { Console.WriteLine("Good Night"); }
+
+            //Console.WriteLine("The Number Jackline is " + Jackline);
+            //Console.WriteLine($"The Number Jackline is {Jackline} + {mariam}");
+            //Console.WriteLine($"The Number Jackline is {Jackline + mariam}");
+            //Console.WriteLine($"{faith} is {mariam - precious}");
+            //Console.WriteLine($"{faith} is {mariam == precious}");
+            //Console.WriteLine($"{faith} is {Dami}");
+        }
+    }
+}
